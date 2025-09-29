6903759c67e739284bd368260b915e1a9cea714e
Obed Okemsinachi <obedokemsinachi@gmail.com>
2025-09-29 05:33:45 +0100

    [fix] cors fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["https://useunisync.vercel.app"], credentials: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
